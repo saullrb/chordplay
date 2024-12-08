5c696977e37edc8f1ee24469ec65b98465e11013
Saull Brandão <saull@outlook.com>
2024-12-08 10:54:32 -0300

    change the Container component and use it on the navbar

--- resources/js/Components/Container.vue
+++ resources/js/Components/Container.vue
@@ -1,3 +1,4 @@
 <template>
-  <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8"><slot /></div>
+    <div class="container mx-auto w-full px-4 sm:px-6 lg:px-8"><slot /></div>
+    <!-- <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8"><slot /></div> -->
 </template>
--- resources/js/Components/PageHeader.vue
+++ resources/js/Components/PageHeader.vue
@@ -1,11 +1,11 @@
 <script setup>
 defineProps({
-  title: String,
+    title: String,
 });
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold text-gray-900 dark:text-white">
-    {{ title }}
-  </h1>
+    <h1 class="text-3xl font-bold text-gray-900 dark:text-white">
+        {{ title }}
+    </h1>
 </template>
