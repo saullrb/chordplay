92759a8a0105d1a3bc793952a71aceaac9a4d0f3
Saull Brandão <saull@outlook.com>
2024-12-08 10:51:50 -0300

    remove application logo component

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -1,24 +1,26 @@
 <script setup>
-import ApplicationLogo from '@/Components/ApplicationLogo.vue';
 import NavBar from '@/Components/NavBar.vue';
 </script>
 
 <template>
-  <div>
-    <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-      <NavBar />
+    <div>
+        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+            <NavBar />
 
-      <!-- Page Heading -->
-      <header class="bg-white shadow dark:bg-gray-800" v-if="$slots.header">
-        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-          <slot name="header" />
+            <!-- Page Heading -->
+            <header
+                class="bg-white shadow dark:bg-gray-800"
+                v-if="$slots.header"
+            >
+                <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+                    <slot name="header" />
+                </div>
+            </header>
+
+            <!-- Page Content -->
+            <main>
+                <slot />
+            </main>
         </div>
-      </header>
-
-      <!-- Page Content -->
-      <main>
-        <slot />
-      </main>
     </div>
-  </div>
 </template>
--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -1,5 +1,4 @@
 <script setup>
-import ApplicationLogo from '@/Components/ApplicationLogo.vue';
 import { Link } from '@inertiajs/vue3';
 </script>
 
@@ -8,9 +7,7 @@ import { Link } from '@inertiajs/vue3';
         class="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0 dark:bg-gray-900"
     >
         <div>
-            <Link href="/">
-                <ApplicationLogo class="h-20 w-20 fill-current text-gray-500" />
-            </Link>
+            <Link href="/"> </Link>
         </div>
 
         <div
