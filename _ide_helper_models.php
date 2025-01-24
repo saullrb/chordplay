998b3a7e74a3b798d020417141c24b87c46f40b6
Saull Brandão <saull@outlook.com>
2025-01-24 18:09:57 -0300

    update to inertia2

--- _ide_helper_models.php
+++ _ide_helper_models.php
@@ -120,6 +120,10 @@ namespace App\Models{
  * @property string|null $remember_token
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Artist> $favoriteArtists
+ * @property-read int|null $favorite_artists_count
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Song> $favoriteSongs
+ * @property-read int|null $favorite_songs_count
  * @property-read \Illuminate\Notifications\DatabaseNotificationCollection<int, \Illuminate\Notifications\DatabaseNotification> $notifications
  * @property-read int|null $notifications_count
  * @property-read \App\Models\Role|null $role
