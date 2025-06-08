20ce419a97933717752813a4fa16a318894a0a7e
Saull Brandão <saull@outlook.com>
2025-06-08 09:14:47 -0300

    feat: add placeholder to better explain the song form

--- resources/js/Pages/Songs/partials/SongForm.vue
+++ resources/js/Pages/Songs/partials/SongForm.vue
@@ -32,28 +32,15 @@ function handleSubmit() {
     <form @submit.prevent="handleSubmit" class="flex w-full flex-col gap-2">
         <div class="flex flex-col gap-1">
             <InputLabel for="name" value="Name" />
-            <input
-                class="px-2 py-1 dark:bg-gray-800 dark:text-white"
-                type="text"
-                required
-                v-model="form.name"
-                id="name"
-            />
+            <input class="px-2 py-1 dark:bg-gray-800 dark:text-white" type="text" required v-model="form.name"
+                id="name" />
             <InputError :message="form.errors?.name" />
         </div>
         <div class="flex flex-col gap-1">
             <InputLabel for="key" value="Key" />
-            <select
-                class="bg-white p-2 dark:bg-gray-800 dark:text-white"
-                v-model="form.key"
-                id="key"
-            >
-                <option
-                    :key="key"
-                    v-for="key in available_keys"
-                    :value="key"
-                    class="px-2 dark:bg-gray-800 dark:text-white"
-                >
+            <select class="bg-white p-2 dark:bg-gray-800 dark:text-white" v-model="form.key" id="key">
+                <option :key="key" v-for="key in available_keys" :value="key"
+                    class="px-2 dark:bg-gray-800 dark:text-white">
                     {{ key }}
                 </option>
             </select>
@@ -61,12 +48,9 @@ function handleSubmit() {
         </div>
         <div class="flex flex-col gap-1">
             <InputLabel for="content" value="Content" />
-            <textarea
-                v-model="form.content"
-                id="content"
-                required
-                class="h-80 resize-y overflow-auto px-2 font-mono text-sm leading-5 tracking-tighter whitespace-pre-wrap dark:bg-gray-800 dark:text-white"
-            ></textarea>
+            <textarea v-model="form.content" id="content" required
+                :placeholder="'Use [] to denote chords. Example -> [C]   [Dm]   [F7].\r\nIf the first non-empty character of the line is not [ , then it will be considered a lyric line.'"
+                class="h-80 resize-y overflow-auto px-2 font-mono text-sm leading-5 tracking-tighter whitespace-pre-wrap dark:bg-gray-800 dark:text-white"></textarea>
         </div>
         <InputError :message="form.errors?.content" />
         <div class="mt-4 flex justify-end gap-2">
