c87c305626c00c62ae52527d3b2418248710c3da
Saull Brandão <saull@outlook.com>
2025-06-09 12:25:56 -0300

    refactor: remove not needed imports

--- resources/js/Components/FavoriteButton.vue
+++ resources/js/Components/FavoriteButton.vue
@@ -1,5 +1,4 @@
 <script setup>
-import { defineProps, defineEmits } from 'vue';
 
 defineProps({ is_favorited: Boolean });
 
@@ -7,16 +6,10 @@ const emit = defineEmits(['favorite']);
 </script>
 
 <template>
-    <button
-        class="cursor-pointer disabled:cursor-wait"
-        @click="() => emit('favorite')"
-    >
-        <i
-            class="fa-regular fa-star text-xl text-black hover:text-yellow-600 dark:text-white"
-            :class="{
-                'fa-solid text-yellow-600 hover:brightness-90 dark:text-yellow-600':
-                    is_favorited,
-            }"
-        ></i>
+    <button class="cursor-pointer disabled:cursor-wait" @click="() => emit('favorite')">
+        <i class="fa-regular fa-star text-xl text-black hover:text-yellow-600 dark:text-white" :class="{
+            'fa-solid text-yellow-600 hover:brightness-90 dark:text-yellow-600':
+                is_favorited,
+        }"></i>
     </button>
 </template>
