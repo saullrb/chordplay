0ac8b14ab5c2900ba1fa45e97d12fe7f1c0f40b8
Saull Brandão <saull@outlook.com>
2025-06-18 08:54:50 -0300

    chore: update database seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Artist;
+use App\Models\Song;
 use App\Models\SongSubmission;
 use App\Models\User;
 use Illuminate\Database\Seeder;
@@ -16,7 +17,10 @@ class DatabaseSeeder extends Seeder
     {
         $user = User::factory()->create();
         $artist = Artist::factory()->create();
-        SongSubmission::factory()->count(10)->create([
+        Song::factory()->count(20)->create([
+            'artist_id' => $artist->id,
+        ]);
+        SongSubmission::factory()->count(5)->create([
             'artist_id' => $artist->id,
             'user_id' => $user->id,
         ]);
