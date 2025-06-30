c8f1bae733a9f2c8bd095ce858ae68a365f7041b
Saull Brandão <saull@outlook.com>
2025-06-30 09:27:05 -0300

    fix: pagination component

--- resources/js/Components/UI/SimplePagination.vue
+++ resources/js/Components/UI/SimplePagination.vue
@@ -15,33 +15,31 @@ defineProps({
 
 <template>
     <ul
-        v-if="pagination.total > 0"
-        class="mt-2 flex items-center justify-center gap-3 text-gray-900 dark:text-white"
+        v-if="pagination.last_page !== 1"
+        class="mt-2 flex items-center justify-center gap-3 dark:text-white"
     >
         <li class="flex items-center justify-center">
             <Link
-                :href="pagination.prevPageUrl ?? ''"
+                :href="pagination.prev_page_url ?? ''"
                 class="btn btn-xs btn-ghost"
                 :class="{
-                    'btn-disabled': !pagination.prevPageUrl,
+                    'btn-disabled': !pagination.prev_page_url,
                 }"
             >
                 <ChevronLeftIconSolid class="size-3" />
             </Link>
         </li>
 
-        <li
-            class="flex items-center justify-center text-sm text-gray-500 dark:text-gray-400"
-        >
-            {{ pagination.currentPage }}/{{ pagination.lastPage }}
+        <li class="flex items-center justify-center text-sm">
+            {{ pagination.current_page }}/{{ pagination.last_page }}
         </li>
 
         <li class="flex items-center justify-center">
             <Link
-                :href="pagination.nextPageUrl ?? ''"
+                :href="pagination.next_page_url ?? ''"
                 class="btn btn-xs btn-ghost"
                 :class="{
-                    'btn-disabled': !pagination.nextPageUrl,
+                    'btn-disabled': !pagination.next_page_url,
                 }"
             >
                 <ChevronRightIconSolid class="size-3" />
